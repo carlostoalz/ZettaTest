5da91b6efde502c5ee04ccb7f00fdc677836a723
Carlos Andres Tobon Alzate <2436725@tcs.com>
2025-04-25 23:56:34 -0500

    input in list

--- Frontend/src/app/pages/time-value/components/list/list.component.ts
+++ Frontend/src/app/pages/time-value/components/list/list.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, inject, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { TableModule } from 'primeng/table';
 import { TimeValue } from '../../../../core/models/TimeValue.model';
@@ -13,21 +20,10 @@ import { TimeValueService } from '../../services/time-value.service';
   styleUrl: './list.component.css',
 })
 export class ListComponent implements OnInit {
+  @Input({ required: true }) timeValues: TimeValue[];
   @Output() launchFormEvent = new EventEmitter<void>();
   private service = inject(TimeValueService);
-  timeValues: TimeValue[] = [];
-  ngOnInit(): void {
-    this.service.getTimeValues().subscribe((tvs) => (this.timeValues = tvs));
-    this.service.getTimeValue().subscribe((timeValue) => {
-      if (timeValue) {
-        this.timeValues = [...this.timeValues];
-        const index = this.timeValues.findIndex((tv) => tv.Id === timeValue.Id);
-        index >= 0
-          ? (this.timeValues[index] = timeValue)
-          : this.timeValues.push(timeValue);
-      }
-    });
-  }
+  ngOnInit(): void {}
   launchForm(): void {
     this.launchFormEvent.emit();
   }
--- Frontend/src/app/pages/time-value/container/time-value.component.ts
+++ Frontend/src/app/pages/time-value/container/time-value.component.ts
@@ -1,6 +1,7 @@
 import { DialogModule } from '@angular/cdk/dialog';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { TimeValue } from '../../../core/models/TimeValue.model';
 import { FormComponent } from '../components/form/form.component';
 import { ListComponent } from '../components/list/list.component';
 import { TimeValueService } from '../services/time-value.service';
@@ -15,8 +16,19 @@ import { TimeValueService } from '../services/time-value.service';
 export class TimeValueComponent implements OnInit {
   private service = inject(TimeValueService);
   private dialog = inject(MatDialog);
+  timeValues: TimeValue[] = [];
   ngOnInit(): void {
     this.service.GetTimeValues();
+    this.service.getTimeValues().subscribe((tvs) => (this.timeValues = tvs));
+    this.service.getTimeValue().subscribe((timeValue) => {
+      if (timeValue) {
+        this.timeValues = [...this.timeValues];
+        const index = this.timeValues.findIndex((tv) => tv.Id === timeValue.Id);
+        index >= 0
+          ? (this.timeValues[index] = timeValue)
+          : this.timeValues.push(timeValue);
+      }
+    });
   }
   launchForm(): void {
     const dialogRef = this.dialog.open(FormComponent, {
